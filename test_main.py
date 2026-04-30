22f9016842cbe518327bc031bce11ff4efb80214
Talita Yuki <talitavila9@gmail.com>
2026-04-30 10:07:24 -0300

    Corrigindo teste da rota de multiplicacao

--- test_main.py
+++ test_main.py
@@ -16,4 +16,4 @@ def test_somar():
 def test_multiplicar():
     response = client.get("/multiplicar/2/2")
     assert response.status_code == 200
-    assert response.json() == {"resultado": 5}
+    assert response.json() == {"resultado": 4}
