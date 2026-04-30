bab9990c82846df5a0ca3f8db943eb602cfbfaac
Talita Yuki <talitavila9@gmail.com>
2026-04-30 09:54:08 -0300

    Projeto inicial FastAPI com CI

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"mensagem": "API funcionando!"}
+
+@app.get("/somar/{a}/{b}")
+def somar(a: int, b: int):
+    return {"resultado": a + b}
